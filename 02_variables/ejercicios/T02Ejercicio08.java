28f36124b567cb97bd9c8cc5b36a5d3550b1dcd1
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-10-27 00:05:41 +0200

    the statements of the exercises T02E07 y T02E08 have been added

--- 02_variables/ejercicios/T02Ejercicio08.java
+++ 02_variables/ejercicios/T02Ejercicio08.java
@@ -1,5 +1,9 @@
 /**
- * Explica tu codigo aqui
+ * Escribe un programa que declare variables de tipo char y de tipo String.
+ * Intenta
+ * mostrarlas por pantalla todas juntas en la misma línea y con una sola
+ * sentencia
+ * de Java (con un solo println) ¿es posible?
  * 
  * @autor Barbara Colomer
  */
