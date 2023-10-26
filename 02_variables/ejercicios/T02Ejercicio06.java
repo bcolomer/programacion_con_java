4692c7476df5640f722c94ab242c191a904345c5
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-10-26 23:55:26 +0200

    T02E06 corrected to have 2 decimal places

--- 02_variables/ejercicios/T02Ejercicio06.java
+++ 02_variables/ejercicios/T02Ejercicio06.java
@@ -8,13 +8,14 @@
 public class T02Ejercicio06 {
   public static void main(String[] args) {
     double precioSinIva = 25.69;
-    double porcentajeIva = 21;
+    int porcentajeIva = 21;
     double eurosIva = (precioSinIva * porcentajeIva) / 100;
     double precioFinal = precioSinIva + eurosIva;
 
-    System.out.println("El precio final de su factura de " +
-        precioSinIva + "€ es de " + precioFinal + "€");
-    System.out.println("La base imponible correspondiente al "
-        + porcentajeIva + "% es de " + eurosIva + "€.");
+    System.out.printf("El precio final de su factura de " +
+        precioSinIva + "€ es de %.2f€\n", precioFinal);
+
+    System.out.printf("La base imponible correspondiente al "
+        + porcentajeIva + "%% es de %.2f€\n", eurosIva);
   }
 }
