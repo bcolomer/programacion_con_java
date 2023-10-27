618e73eeb2d857778c2f6b276536580b50ae7a5f
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-10-27 09:52:52 +0200

    T03E07 pruebas

--- 02_variables/ejercicios/T02Ejercicio07.java
+++ 02_variables/ejercicios/T02Ejercicio07.java
@@ -9,6 +9,16 @@
  */
 public class T02Ejercicio07 {
   public static void main(String[] args) {
-
+    // char variable1 = "a"; char no se pueden asignar con comillas dobles
+    char variable2 = 'b';
+    String variable3 = "Esto es una cadena con comillas dobles";
+    // String variable4 = 'Esto es una cadena con comillas simples'; String no se
+    // pueden asignar con comillas simples
+    // System.out.println(variable1);
+    System.out.println(variable2);
+    System.out.println(variable3);
+    // System.out.println(variable4);
+    System.out.println("mostrando por pantalla una variable tipo char: " + variable2 + " y 
+    una variable tipo String en la misma linea:"+variable3);
   }
 }
