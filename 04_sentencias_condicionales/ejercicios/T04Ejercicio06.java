36e7b6ef74489a4bf0ca99583232aacc6a60bba4
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-11-01 15:54:59 +0100

    add t04e06 y t04e07

---
+++ 04_sentencias_condicionales/ejercicios/T04Ejercicio06.java
@@ -0,0 +1,26 @@
+
+/*  
+ * Realiza un programa que calcule el tiempo que tardará en caer un objeto desde
+ * una altura h. Aplica la fórmula t = √ 2h g siendo g = 9:81m/s2
+ * 
+ * @autor Barbara Colomer
+ */
+import java.util.Scanner;
+
+public class T04Ejercicio06 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Ingrese la altura  en metros para saber cuando tiempo tardara en caer un objeto");
+    double h = s.nextDouble();
+    s.close();
+    if (h <= 0) {
+      System.out.println("Por favor introduzca un valor positivo");
+    } else {
+      double tiempo = Math.sqrt((2 * h) / 9.81);
+
+      System.out.printf("El tiempo que el objeto tardará en caer al suelo es de %.2f segundos.", tiempo);
+
+    }
+  }
+
+}
---
+++ 04_sentencias_condicionales/ejercicios/T04Ejercicio07.java
@@ -0,0 +1,25 @@
+
+/*
+ * Realiza un programa que calcule la media de tres notas
+ * 
+ * @autor Barbara Colomer
+ */
+import java.util.Scanner;
+
+public class T04Ejercicio07 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Introduce por teclado las notas de los 3 ultimos examenes separadas por espacio");
+    double nota1 = s.nextDouble();
+    double nota2 = s.nextDouble();
+    double nota3 = s.nextDouble();
+    s.close();
+    double promedio;
+    if ((nota1 < 0) || (nota1 > 10) || (nota2 < 0) || (nota2 > 10) || (nota3 < 0) || (nota3 > 10)) {
+      System.out.println("Introduce notas validas");
+    } else {
+      promedio = ((nota1 + nota2 + nota3) / 3);
+      System.out.printf("La media de las notas ingresadas es %.2f. ", promedio);
+    }
+  }
+}
