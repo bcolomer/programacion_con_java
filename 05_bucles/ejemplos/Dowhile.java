51c852e41e03468ade40b144250f8cf8b09b72ad
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-11-09 19:26:14 +0100

    T04e09 fixed

--- 05_bucles/ejemplos/Dowhile.java
+++ 05_bucles/ejemplos/Dowhile.java
@@ -1,7 +1,7 @@
 
 /*
  * Escribe un programa que lea desde teclado un numero indefinido de numeros hasta que el usuario introduzca 
- * un valor negativo, momento en el cual se debera mostrar por pantalla el total de numeros leidos y la suma
+ * cero, momento en el cual se debera mostrar por pantalla el total de numeros leidos y la suma
  * de todos ellos. 
  * 
  * @autor Barbara Colomer
@@ -16,14 +16,12 @@ public class Dowhile {
     Scanner s = new Scanner(System.in);
     System.out.println("introduzca numeros");
     do {
-
       num = s.nextInt();
       suma = suma + num;
       contador++;
-
     } while (num != 0);
     System.out.println("contador: " + (contador - 1));
-    System.out.println("suma es :" + suma);
+    System.out.println("suma es: " + suma);
     s.close();
   }
 
