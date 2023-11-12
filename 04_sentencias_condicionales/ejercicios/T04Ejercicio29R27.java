9fa4fa6591ee2ff1e5d286723781f01c8efc19e2
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-11-12 10:56:07 +0100

    add T04Ejercicio29 Moodle 27

--- 04_sentencias_condicionales/ejercicios/T04Ejercicio29R27.java
+++ 04_sentencias_condicionales/ejercicios/T04Ejercicio29R27.java
@@ -9,5 +9,59 @@
  * @autor Barbara Colomer
  */
 public class T04Ejercicio29R27 {
+  public static void main(String[] args) {
+    String comer = "";
+    String beber = "";
+    double precioComer = 0;
+    double precioBeber = 0;
+    double sumaTicket = 0;
 
+    System.out.println("---Cafeteria ALAN TURING---");
+    System.out.print("¿Que ha pedido para comer? palmera, donut o pitufo: ");
+    comer = System.console().readLine().toLowerCase();
+    if (comer.equals("pitufo")) {
+      System.out.print("lo ha pedido con aceite o tortilla: ");
+      comer = System.console().readLine();
+    }
+    System.out.print("¿Que ha pedido de bebida? zumo o cafe: ");
+    beber = System.console().readLine().toLowerCase();
+    if ((comer.equals("palmera")) || (comer.equals("donut")) || (comer.equals("tortilla"))
+        || (comer.equals("aceite"))) {
+      if (comer.equals("palmera")) {
+        comer = "Palmera: ";
+        precioComer = 1.4;
+      } else if (comer.equals("donut")) {
+        comer = "Donut: ";
+        precioComer = 1;
+      } else if (comer.equals("aceite")) {
+        comer = "Pitufo con aceite: ";
+        precioComer = 1.2;
+
+      } else if (comer.equals("tortilla")) {
+        comer = "Pitufo con tortilla: ";
+        precioComer = 1.6;
+      }
+      System.out.printf("%-25s %-6.2f€\n", comer, precioComer);
+    } else {
+      System.out.println("La comida introducida no es correcta.");
+    }
+    if ((beber.equals("cafe")) || (beber.equals("zumo"))) {
+      if (beber.equals("cafe")) {
+        beber = "Café: ";
+        precioBeber = 1.2;
+      } else if (beber.equals("zumo")) {
+        beber = "Zumo: ";
+        precioBeber = 1.5;
+      }
+      System.out.printf("%-25s %-6.2f€\n", beber, precioBeber);
+
+    } else {
+      System.out.println("La bebida introducida no es correcta.");
+    }
+    // System.out.printf("%-25s %-6.2f€\n", comer, precioComer);
+    // System.out.printf("%-25s %-6.2f€\n", beber, precioBeber);
+    System.out.printf("%-25s %-6.2f€\n", "Total desayuno: ", (precioBeber + precioComer));
+    System.out.println("Gracias por su compra!");
+
+  }
 }
