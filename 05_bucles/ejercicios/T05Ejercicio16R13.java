8f4505be1564347c1062e59fdd7a232fb1a64237
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-11-25 14:04:36 +0100

    t05 ejercicio16 relacion 13

---
+++ 05_bucles/ejercicios/T05Ejercicio16R13.java
@@ -0,0 +1,36 @@
+
+/*
+ * Escribe un programa que diga si un número introducido por teclado es o no primo. 
+ * Un número primo es aquel que sólo es divisible entre él mismo y la unidad. 
+ * 
+ * @autor Barbara Colomer
+ * 
+ */
+import java.util.Scanner;
+
+public class T05Ejercicio16R13 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    boolean primo = false;
+    int num = 0;
+    int esPrimo;
+
+    System.out.println("Introduzca un número para saber si es o no primo");
+    num = s.nextInt();
+
+    for (int i = num - 1; i >= 2; i--) {
+      if (num % i == 0) {
+        primo = true;
+      }
+
+    }
+    s.close();
+    if (primo) {
+      System.out.println("El numero no es primo");
+    } else {
+      System.out.println("El numero introducido es primo");
+
+    }
+
+  }
+}
