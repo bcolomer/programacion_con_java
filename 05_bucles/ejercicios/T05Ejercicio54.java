840c0f2d8a364d71fa2dd184fc4be9a8fc14e3db
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-06 15:13:42 +0100

    add T05ejercicio54

---
+++ 05_bucles/ejercicios/T05Ejercicio54.java
@@ -0,0 +1,36 @@
+/*
+ * Realiza un programa que pinte un triángulo hueco tal como se muestra en
+ * los ejemplos. El usuario debe introducir la altura de la figura. 
+ * 
+ * @autor Barbara Colomer
+ */
+
+import java.util.Scanner;
+
+public class T05Ejercicio54 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int altura = 0;
+    int anchura = 0;
+
+    System.out.println("Introduzca la altura para el dibujar el triangulo hueco");
+    altura = s.nextInt();
+    s.close();
+    anchura = altura;
+
+    for (int i = 0; i < altura; i++) {
+      for (int j = 0; j < anchura; j++) {
+
+        if (((i != 0) && (i < altura - 2)) && ((j < anchura - 1) && (j != 0))) {
+          System.out.print(" ");
+        } else {
+          System.out.print("*");
+        }
+
+        // System.out.print("x");
+      }
+      anchura = anchura - 1;
+      System.out.println();
+    }
+  }
+}
