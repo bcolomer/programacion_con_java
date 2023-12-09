34e2b130e2123981b30aaa72a631c09636e1a7f3
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-09 10:24:12 +0100

    rehago el la piramide de numeros crecientesdecrecientes

--- 05_bucles/ejercicios/T05Ejercicio24R21.java
+++ 05_bucles/ejercicios/T05Ejercicio24R21.java
@@ -13,25 +13,50 @@ public class T05Ejercicio24R21 {
     System.out.println("escribe la altura para la piramide");
     altura = s.nextInt();
     s.close();
-    int fila;
-    int columna;
-    for (fila = 1; fila <= altura; fila++) {
 
-      // escribir espacios
-      for (columna = 1; columna <= (altura - fila); columna++) {
+    for (int i = 0; i < altura; i++) {
+      // esto pinta espacios:
+      for (int j = 0; j < altura - i; j++) {
         System.out.print("_");
+      }
+      // esto imprime una piramide normal con numeros ascendentes
+      /*
+       * for (int j = 1; j <= (i * 2) + 1; j++) {
+       * System.out.print(j);
+       * }
+       */
+      // esto imprime la primera mitad de la piramide
+      for (int j = 1; j <= i + 1; j++) {
+        System.out.print(j);
+      }
+      for (int j = i; j > 0; j--) {
+        System.out.print(j);
+      }
 
-      }
-      // escribir numeros crecientes
-      for (columna = 1; columna <= fila; columna++) {
-        System.out.print(columna);
-      }
-
-      // escribir numeros decrecientes
-      for (columna = fila - 1; columna >= 1; columna--) {
-        System.out.print(columna);
-      }
-      System.out.println("");
+      System.out.println();
     }
+
+    /*
+     * int fila;
+     * int columna;
+     * for (fila = 1; fila <= altura; fila++) {
+     * 
+     * // escribir espacios
+     * for (columna = 1; columna <= (altura - fila); columna++) {
+     * System.out.print("_");
+     * 
+     * }
+     * // escribir numeros crecientes
+     * for (columna = 1; columna <= fila; columna++) {
+     * System.out.print(columna);
+     * }
+     * 
+     * // escribir numeros decrecientes
+     * for (columna = fila - 1; columna >= 1; columna--) {
+     * System.out.print(columna);
+     * }
+     * System.out.println("");
+     * }
+     */
   }
 }
