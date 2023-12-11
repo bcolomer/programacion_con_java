dc049c48829bc520c66c5f7607970aee82b77230
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2023-12-11 10:27:13 +0100

    add examen 12345

---
+++ examen_java_1_2_3_4_5/Ejercicio1.java
@@ -0,0 +1,116 @@
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+  public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+    long numero = 0;
+    char color;
+    long invertido = 0;
+    long descomponer = 0;
+    long modulo = 0;
+    int contador = 0;
+    int copiaContador = 0;
+
+    System.out.println("GRAPHIFY");
+    System.out.println("============================");
+    System.out.print("Introduce un numero: ");
+    do {
+      numero = s.nextLong();
+      if (numero <= 0) {
+        System.out.println("ERROR: el número tiene que ser mayor que cero.");
+        System.out.println("Introduzca el numero nuevamente:");
+
+      }
+    } while (numero <= 0);
+    System.out.println("Dibujo la gráfica en blanco y negro o en color (B/C)?");
+    color = s.next().charAt(0);
+
+    s.close();
+
+    invertido = Invertir(numero);
+    contador = ContadorDigitos(numero);
+    copiaContador = contador;
+
+    do {
+      modulo = invertido % 10;
+      invertido = invertido / 10;
+      descomponer = (descomponer * 10) + modulo;
+
+      // este for imprime tantas lineas como digitos tenga el numero
+      for (int i = 0; i < copiaContador; i++) {
+
+        // esto imprime las lineas arriba
+
+        for (int j = 0; j <= 9; j++) {
+          System.out.print(" ---");
+        }
+        System.out.println();
+
+        System.out.printf("| %d |", modulo);
+
+        // esto imprimira las lineas del medio
+        for (int j = 0; j < 9; j++) {
+
+          if (j < modulo) {
+            System.out.print(" * |");
+          } else {
+            System.out.print("   |");
+          }
+
+        }
+
+        copiaContador--;
+        System.out.println();
+      }
+
+    } while (invertido > 0);
+    // esto imprime las lineas abajo
+    for (int k = 0; k <= 9; k++) {
+      System.out.print(" ---");
+    }
+  }
+
+  /**
+   * funcion para invertir un numero tipo long
+   * 
+   * @param numero
+   * @return
+   */
+  public static long Invertir(long numero) {
+    long modulo = 0;
+    long invertido = 0;
+
+    do {
+      modulo = numero % 10;
+      numero = numero / 10;
+      invertido = (invertido * 10) + modulo;
+
+    } while (numero > 0);
+
+    return invertido;
+  }
+
+  /**
+   * funcion para contar digitos de un numero tipo long
+   * 
+   * @param numero
+   * @return
+   */
+  public static int ContadorDigitos(long numero) {
+    long modulo = 0;
+    long invertido = 0;
+    int contador = 0;
+
+    do {
+      modulo = numero % 10;
+      numero = numero / 10;
+      invertido = (invertido * 10) + modulo;
+      contador++;
+
+    } while (numero > 0);
+
+    return contador;
+  }
+}
