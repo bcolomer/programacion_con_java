a7735e75ff92cf3c898923a4bb20594a361f2464
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2024-02-03 00:32:57 +0100

    terminado bidim 2 r24

--- 07_arrays/ejercicios/T072Ejercicio02R24.java
+++ 07_arrays/ejercicios/T072Ejercicio02R24.java
@@ -1,16 +1,16 @@
 /*
- * Escribe un programa que pida 20 números enteros. Estos números se deben
+* Escribe un programa que pida 20 números enteros. Estos números se deben
 introducir en un array de 4 filas por 5 columnas. El programa mostrará las
 sumas parciales de filas y columnas igual que si de una hoja de cálculo se
 tratara. La suma total debe aparecer en la esquina inferior derecha. 
- * 
- * @autor Barbara Colomer
- */
+* 
+* @autor Barbara Colomer
+*/
 public class T072Ejercicio02R24 {
     public static void main(String[] args) {
         int[][] numeros;
         numeros = new int[4][5];
-        // FIXME corregir el tema de la suma de las filas y columnas
+
         // crearArray(numeros);
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
@@ -21,20 +21,13 @@ public class T072Ejercicio02R24 {
 
         System.out.printf("\033[H\033[2J");
         System.out.flush();
+
         mostrarArray(numeros);
         System.out.println();
-        for (int i = 0; i <= numeros.length; i++) {
-            for (int j = 0; j <= numeros[i].length; j++) {
-                if (i == numeros[i].length) {
-                    System.out.print(sumarColumnas(numeros, j) + " ");
-                    // } else if (j == numeros[i].length) {
-                    // System.out.print(sumarFilas(numeros, i) + " ");
-                } else {
-                    System.out.print(" j:" + j + " " + numeros[i][j]);
-                }
-            }
-            System.out.println();
-        }
+        // muestro array sumando las filas y luego muestro la suma de columnas y total
+        mostrarArraySumandoFilas(numeros);
+        mostrarSumaColumnas(numeros);
+
         // System.out.println("fila 0 : " + sumarFilas(numeros, 0));
         // System.out.println("fila 1 : " + sumarFilas(numeros, 1));
         // System.out.println("fila 2 : " + sumarFilas(numeros, 2));
@@ -49,6 +42,8 @@ public class T072Ejercicio02R24 {
 
     /**
      * suma las filas del array
+     * no lo he utilizado, he usado otra forma de sumar filas mientras mostraba el
+     * array
      * 
      * @param numeros
      * @param control
@@ -119,29 +114,45 @@ public class T072Ejercicio02R24 {
     }
 
     /**
-     * mostrar array con las filas y las columnas sumadas
+     * mostrar array con las filas sumadas
      * 
      * @param numeros
      */
-    public static void mostrarArraySumado(int[][] numeros) {
-        for (int fila = 0; fila <= numeros.length; fila++) {
+    public static void mostrarArraySumandoFilas(int[][] numeros) {
+        int suma = 0;
+        for (int fila = 0; fila < numeros.length; fila++) {
 
-            for (int columna = 0; columna <= numeros[fila].length; columna++) {
-                if (columna == numeros[fila].length) {
-                    System.out.print(sumarColumnas(numeros, columna));
-                }
+            for (int columna = 0; columna < numeros[fila].length; columna++) {
+                System.out.print(numeros[fila][columna] + "  ");
+                suma += numeros[fila][columna];
             }
+            System.out.println(" " + suma);
+            suma = 0;
+            System.out.println();
         }
         System.out.println();
     }
 
-    // System.out.println("fila 1 : " + sumarFilas(numeros, 1));"\u2211"
-    // System.out.println("fila 2 : " + sumarFilas(numeros, 2));
-    // System.out.println("fila 3 : " + sumarFilas(numeros, 3));
-    // System.out.println("Columna 0 : " + sumarColumnas(numeros, 0));
-    // System.out.println("Columna 1 : " + sumarColumnas(numeros, 1));
-    // System.out.println("Columna 2 : " + sumarColumnas(numeros, 2));
-    // System.out.println("Columna 3 : " + sumarColumnas(numeros, 3));
-    // System.out.println("Columna 4 : " + sumarColumnas(numeros, 4));
+    /**
+     * mostrar las columnas sumadas y el total de la suma de las filas
+     * 
+     * @param numeros
+     */
+    public static void mostrarSumaColumnas(int[][] numeros) {
+        int suma = 0;
+        int sumaTotal = 0;
+        for (int fila = 0; fila < numeros.length; fila++) {
+            for (int columna = 0; columna < numeros[fila].length; columna++) {
+                suma += numeros[fila][columna];
+                if (fila == 0) {
+                    System.out.print(sumarColumnas(numeros, columna) + " ");
+                }
+            }
+            sumaTotal += suma;
+            suma = 0;
+        }
+        System.out.print("  " + sumaTotal);
+        System.out.println();
+    }
 
 }
